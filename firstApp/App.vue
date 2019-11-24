74cde5fe65513ca1b111fe3986ecf0babe0ca5bb
Alejandro Sobko <alejandro.sobko@gmail.com>
2019-11-24 01:19:01 -0300

    Add geolocation

--- firstApp/App.vue
+++ firstApp/App.vue
@@ -1,6 +1,6 @@
 <template>
   <view  class="container">
-    <MapView class="container" :initial-region="coordinates">
+    <MapView class="container" :initial-region="userLocation" v-if="loaded">
       <Marker v-for="marker in markers"
               v-bind:key="marker.id"
               :coordinate="marker.coordinates"
@@ -13,28 +13,46 @@
 <script>
 import MapView from "react-native-maps";
 import { Marker } from 'react-native-maps';
-
+import * as Location from "expo-location";
+import * as Permissions from 'expo-permissions';
 
 export default {
+  beforeMount: function() {
+    this.getLocation();
+  },
   data: function() {
     return {
+      loaded: false,
       markers: [
         { id: 1, title: "Ubicación 1", description: "Esta es una descripción", coordinates: {latitude: -34.724609, longitude: -58.260281} },
         { id: 2, title: "Ubicación 2", description: "Esta es una descripción", coordinates: {latitude: -34.824609, longitude: -58.360281} },
         { id: 3, title: "Ubicación 3", description: "Esta es una descripción", coordinates: {latitude: -34.924609, longitude: -58.460281} },
         { id: 4, title: "Ubicación 4", description: "Esta es una descripción", coordinates: {latitude: -34.100609, longitude: -58.560281} },
       ],
-      coordinates: {
-        latitude: -34.724609,
-        longitude: -58.260281,
+      userLocation: {
         latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421
+        longitudeDelta: 0.0421,
       },
     };
   },
   components: {
     MapView,
     Marker,
+  },
+  methods: {
+    getLocation: function() {
+      Permissions.getAsync(Permissions.LOCATION).then(userPermission => {
+        if (userPermission.status !== "granted") { return; }
+
+        Location.getCurrentPositionAsync({}).then(location => {
+          this.userLocation.latitude = location.coords.latitude;
+          this.userLocation.longitude = location.coords.longitude;
+          this.loaded = true;
+        });
+      }).catch((err)=>{
+        console.log(err);
+     });
+    }
   }
 };
 </script>
